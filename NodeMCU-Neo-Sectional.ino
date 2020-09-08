6e166d68a11dcfe0c228cc21ded879ec4610e292
Rich Hopkins <Rich-Hopkins@users.noreply.github.com>
2020-09-08 09:32:25 -0400

    Update NodeMCU-Neo-Sectional.ino
    Remove unneeded include

--- NodeMCU-Neo-Sectional.ino
+++ NodeMCU-Neo-Sectional.ino
@@ -2,7 +2,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-//#include <WiFiClient.h>
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
 #include <ezTime.h>
